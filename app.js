f42d39a466e2afafa23c89d97f20164403884a37
abdulcodess <abdult7411@gmail.com>
2025-10-15 21:28:17 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,7 +1,2 @@
 // new feature to add - form
-<<<<<<< HEAD
 // new feature to add - button
-=======
-// new feature to add - button
-// upgarding more
->>>>>>> 6f2e4703200a4935b28b3567ffb5079e843033c9
